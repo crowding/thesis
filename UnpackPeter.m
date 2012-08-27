619baa422ab2884cacd6bbc47afc550179f66e40
Peter Meilstrup <peter.meilstrup@gmail.com>
2012-08-27 03:19:56 -0700

    generalize unpack for future datasets

--- UnpackPeter.m
+++ UnpackPeter.m
@@ -1,4 +1,4 @@
-% UnpackPeter.m
+function UnpackPeter(varargin)
 
 % Here's CSV files with individual trial data.
 % 
@@ -44,13 +44,23 @@
 % their response latency were in the window. Some subjects' responses
 % varied with the response latency, which isn't included in this data.
 
-contentData = read_csv('content_series_trials.csv');
-spacingData = read_csv('spacing_series_trials.csv');
+if isempty(varargin)
+    d = dir('*_trials.csv');
+    varargin = {d.name};
+end
 
-fields = fieldnames(contentData);
-allData = cellfun(@(x) struct2cell(orderfields(x, fields)), ...
-                  {contentData, spacingData}, 'UniformOutput', 0)
-allData = cell2struct(cellfun(@vertcat, allData{:}, 'UniformOutput', 0), fields, 1) 
+S = struct();
 
-save data allData contentData spacingData
+cellfun(@loadFile, varargin)
+function loadFile(filename)
+    groupname = regexp(filename, '([a-zA-Z])*_series_trials.*', 'tokens');
+    S.([groupname{1}{1} 'Data']) = read_csv(filename);
+end
 
+fields = fieldnames(S.contentData);
+S.allData = cellfun(@(x) struct2cell(orderfields(x, fields)), ...
+                  {S.contentData, S.spacingData}, 'UniformOutput', 0);
+S.allData = cell2struct(cellfun(@vertcat, S.allData{:}, 'UniformOutput', 0), fields, 1);
+
+save('data.mat', '-struct', 'S')
+end
