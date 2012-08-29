87d19526deee8316346da8d08d0b4b3bf95eecf9
Peter Meilstrup <peter.meilstrup@gmail.com>
2012-08-29 03:16:52 -0700

    Pull initial parameters out

--- MeilstrupBoyntonModel.m
+++ MeilstrupBoyntonModel.m
@@ -1,4 +1,6 @@
-function MeilstrupBoyntonModel
+function MeilstrupBoyntonModel(1)
+%if given value of 1, will assign everything to base workspace to
+%play with.
 
 % Notes 
 % as is hard to fit all without c0 = -.1
@@ -21,52 +23,9 @@ expType = 'spacing';
 
 subject = 'pbm';
 
-%Initial parameters
-switch subject
-    case 'as'
-        p.sig0 = .065;
-        p.sigk = .75;
-        p.siga = 3;
-        
-        p.mua = 2;
-        p.mukc = 1.5;
-        p.muks = 1.5;
-        p.mu0 = -.1;
-        
-    case 'jb'
-        p.sig0 = .1;
-        p.sigk = .75;
-        p.siga = .5;
-        
-        p.mua = 1;
-        p.mukc = .5;
-        p.muks = 0;
-        p.mu0 = -.2;
-    otherwise
-        % These are some pretty good initial parameters for most subjects
-%         p.sig0 = .065;
-%         p.sigk = 2;
-%         p.siga = 11;
-%         
-%         p.mua = 3.2;
-%         p.mukc = 2;
-%         p.muks = 7.7;
-%         p.mu0 = 0;
-        
-        p.siga = 3;
-        p.sigk = .75;
-        p.sig0 = .75;
-        
-        p.mua = 8;
-        p.muks = 1;
-        p.mukc = 1;
-        p.mu0 = 0;
-         
-end
+p = initialParams(subject);
+
 freeParams = {'sig0','sigk','siga','mua','mukc','muks'};
-
-%freeParams = {'sigk','siga','mua','mukc','muks'};
-
 
 % Load the data if it isn't already loaded ('see UnpackPeter.m')
 S = load('data');
