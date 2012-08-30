342ba96b3b921ec69050745ad438ceb6425cdc16
Peter Meilstrup <peter.meilstrup@gmail.com>
2012-08-29 21:26:20 -0700

    Load into a single dataset.
    For now, I am leaving the existing struct-loading.

--- UnpackPeter.m
+++ UnpackPeter.m
@@ -1,13 +1,18 @@
 function UnpackPeter(varargin)
+% loads all the data into structs.
+% output variables are S, a struct containing all the experiment
+% types, and 'data', in which experiment type is another column.
 
 if isempty(varargin)
     d = dir('*_trials.csv');
-    varargin = {d.name};
+    filenames = {d.name};
+else
+    filenames = varargin;
 end
 
 S = struct();
 
-cellfun(@loadFile, varargin)
+cellfun(@loadFile, filenames)
 function loadFile(filename)
     groupname = regexp(filename, '([a-zA-Z])*_series_trials.*', 'tokens');
     S.([groupname{1}{1} 'Data']) = read_csv(filename);
@@ -16,7 +21,19 @@ end
 fields = fieldnames(S.contentData);
 S.allData = cellfun(@(x) struct2cell(orderfields(x, fields)), ...
                   {S.contentData, S.spacingData}, 'UniformOutput', 0);
-S.allData = cell2struct(cellfun(@vertcat, S.allData{:}, 'UniformOutput', 0), fields, 1);
+S.allData = cell2struct(cellfun(@vertcat, S.allData{:}, 'UniformOutput', 0)...
+                        , fields, 1);
+
+%or, with datasets
+data = cellfun(@load_data_set, filenames, 'UniformOutput', 0);
+function d = load_data_set(filename)
+    groupname = regexp(filename, '([a-zA-Z])*_series_trials.*', 'tokens');
+    d = dataset(read_csv(filename));
+    d.exp_type = repmat(groupname, size(d,1),1);
+end
+
+S.data = cat(1, data{:});
 
 save('data.mat', '-struct', 'S')
+
 end
