1daec0f050661a1ae47229301a878855bffddbb2
Peter Meilstrup <peter.meilstrup@gmail.com>
2013-05-21 07:40:44 -0700

    actually we are using matlab for nothing anymore.

--- countdata.m
+++
@@ -1,9 +0,0 @@
-function count = countdata(data, split)
-
-%This is the matlab translation of this R code.
-%
-%count = ddply(data, split, function(x) c(n=nrow(x), n.yes = sum(x.response), p = mean(x.response)
-
-    count = grpstats(data, split, @(x)mean(logical(x)), 'DataVars', 'response');
-    count = rename(count, 'GroupCount', 'n', 'Fun1_response', 'p');
-end
