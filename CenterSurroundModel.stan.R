9dab7686886abf8af9e01a91d7e4dca2831c920b
Peter Meilstrup <peter.meilstrup@gmail.com>
2013-07-13 07:50:23 -0700

    Fixed center-surround model with predictor

--- CenterSurroundModel.stan.R
+++ CenterSurroundModel.stan.R
@@ -49,53 +49,98 @@ parameters {
   real<upper=0> repulsion;
   real<lower=0, upper=pi()> summation_width;
   real<lower=0, upper=pi()> repulsion_width;
-  //real center_nonlinearity;
+  real nonlinearity;
 }
 
 model {
-  real crowdedness;
-  real link_displacement;
-  real link_summation;
-  real link_repulsion;
-  real link;
-  real response;
-
   for (n in 1:N) {
+    real crowdedness;
+    real link_displacement;
+    real link_summation;
+    real link_repulsion;
+    real link_nonlinearity;
+    real link;
+    real response;
+
     link_summation <- 0;
     link_repulsion <- 0;
     crowdedness <- 2 - 2/(1+exp(-cs/frac_spacing[n]));
     link_displacement <- beta_dx * displacement[n] * crowdedness;
 
-    //compute the summation by Gaussian center-surrounds
-    for (t in 1:target_number_shown[n]) {
-        real loc_t;
-        loc_t <- spacing[n] * t;
-        for (q in 1:target_number_shown[n]) {
-            real loc_q;
-            loc_q <- spacing[n] * q;
-            link_summation <-
-               (link_summation + repulsion *
-                   exp(normal_log(loc_t, loc_q, summation_width)));
-            link_repulsion <-
-               (link_repulsion + repulsion *
-                   exp(normal_log(loc_t, loc_q, repulsion_width)));
+    for (d in 0:target_number_shown[n]-1) {
+        real distance;
+        real multiplicity;
+        //add up center-surround influence between every pair of elements
+        //separated by d spaces
+        real dd;
+        if ( d > target_number_all[n] - d) {
+            dd <- target_number_all[n] - d;
+        } else {
+            dd <- d;
+        };
+        distance <- frac_spacing[n] * dd;
+        if (d == 0) {
+            //an element only influences itself once
+            multiplicity <- target_number_shown[n];
+        } else {
+            //separated elements mutually influence, so twice
+            multiplicity <- 2 * (target_number_shown[n] - dd);
         }
+        link_summation <- link_summation
+          + summation * multiplicity * content[n]
+            * exp(normal_log(distance, 0, summation_width));
+        link_repulsion <- link_repulsion
+          + repulsion * multiplicity * content[n]
+            * exp(normal_log(distance, 0, repulsion_width));
     }
-    link <- bias + link_displacement + link_summation;
+    //Average the influence over each element
+    link_summation <- link_summation / target_number_shown[n];
+    link_repulsion <- link_repulsion / target_number_shown[n];
+    link_nonlinearity <- link_nonlinearity * content[n] * abs(content[n]);
+
+    link <- bias + link_displacement + link_summation + link_repulsion + link_nonlinearity;
     response <- inv_logit( link ) .* (1-lapse) + lapse/2;
     n_cw[n] ~ binomial( n_obs[n], response);
   }
 }'
 
  stan_predict <- mkchain[., coefs](
-     mutate(frac_spacing = 2*pi/target_number_all)
+     subset(., select=intersect(relevant, names(.)))
+   , mutate(frac_spacing = 2*pi/target_number_all)
+   , mutate(., .n=1:nrow(.))
+   , ddply(.,c("target_number_shown", "target_number_all"), function(group) {
+     d <- 0:(group$target_number_shown[1] - 1)
+     d <- pmin(d, group$target_number_all[1] - d)
+     distance <- d * group$frac_spacing[1]
+     multiplicity <- ifelse(
+       d==0,
+       group$target_number_shown[1], 2*(group$target_number_shown[1] - d))
+     raw_summation <- (
+         rep(1, length(multiplicity))
+         %*% matrix(dnorm(distance, 0,
+                          rep(coefs$summation_width, length(distance)))
+                    * multiplicity,
+                    nrow=length(distance)))
+     raw_repulsion <- (
+         rep(1, length(multiplicity))
+         %*% matrix(dnorm(distance, 0,
+                          rep(coefs$repulsion_width, length(distance)))
+                    * multiplicity,
+                    nrow=length(distance)))
+     cbind(group, data.frame(
+         link_summation =
+             (group$content * coefs$summation
+              * as.vector(raw_summation)/group$target_number_shown)
+       , link_repulsion =
+             (group$content * coefs$repulsion
+              * as.vector(raw_repulsion)/group$target_number_shown)
+         ))
+   })
+   , .[order(.$.n), ]
    , with(coefs, summarize(
        .
-     , link_displacement = (beta_dx * displacement
-                            * (2 - 2/(1+exp(-cs/frac_spacing))))
-     , link_repulsion = (repulsion * content
-                         + nonlinearity * (content * abs(content)))
-     , link_summation = (target_number_all * content * summation)
+     , link_displacement = beta_dx * displacement
+                             * (2 - 2/(1+exp(-cs/frac_spacing)))
      , link = bias + link_displacement + link_repulsion + link_summation
      , response = plogis(link) * (1-lapse) + lapse/2
-     )))
+       )))
